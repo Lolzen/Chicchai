256cdd1a63c5968b4038f51c2c6b6ed6fd0d4b31
Georg Eitler <funer@gmx.at>
2017-12-13 23:46:42 +0100

    core: overhaul
    also fix chatevent triggering any chatframe

--- core.lua
+++ core.lua
@@ -1,245 +1,140 @@
---[[			Chicchai
-]]--	by Lolzen & Cargor (EU-Nozdormu)
+--// Chicchai --
+-- by Lolzen & Cargor (EU-Nozdormu) aka xConStruct
 
 local _, ns = ...
 
--- Configuration
-local maxHeight = 120				-- How high the chat frames are when maximized
-local animTime = 0.3				-- How lang the animation takes (in seconds)
-local minimizeTime = 10				-- Minimize after X seconds
-local minimizedLines = 1			-- Number of chat messages to show in minimized state
-
-local MaximizeOnEnter = true		-- Maximize when entering chat frame, minimize when leaving
-local WaitAfterEnter = 0			-- Wait X seconds after entering before maximizing
-local WaitAfterLeave = 0			-- Wait X seconds after leaving before minimizing
-
-local LockInCombat = nil			-- Do not maximize in combat
-
-local MaximizeCombatLog = true		-- When the combat log is selected, it will be maximized
-
--- Modify this to maximize only on special channels
--- comment/remove it to react on all channels
--- you still need the "channel"-event on your chat frame!
-local channelNumbers = {
-	[1] = true,
-	[2] = true,
-	[3]  = true,
-}
-
-local ChatFrameConfig = {	-- Events which maximize the chat for the different windows
-	["ChatFrame1"] = {
-		"say", "emote", "text_emote",
-		"party", "party_leader", "party_guide",
-		"whisper",
-		"guild", "officer",
-		"battleground", "battleground_leader",
-		"raid", "raid_leader", "raid_warning",
-	
-		"bn_whisper",
-		"bn_conversation",
-		"bn_broadcast",
-	},
-	["ChatFrame3"] = true, -- "true" just makes this frame available for minimizing and registers it with Chicchai
-}
-
---[[
-	REFERENCE LIST
-	These are the available chat events for ChatFrameConfig
-		say, yell, emote, text_emote,
-		party, party_leader, party_guide,
-		whisper, whisper_inform, afk, dnd, ignored,
-		guild, officer,
-		channel, channel_join, channel_leave, channel_list, channel_notice, channel_notice_user,
-		battleground, battleground_leader,
-		raid, raid_leader, raid_warning,
-
-		bn_whisper, bn_whisper_inform,
-		bn_conversation, bn_conversation_notice, bn_conversation_list,
-		bn_alert,
-		bn_broadcast, bn_broadcast_inform,
-		bn_inline_toast_alert, bn_inline_toast_broadcast, bn_inline_toast_broadcast_inform, bn_inline_toast_conversation,
-
-		system, achievement, guild_achievement,
-		bg_system_neutral, bg_system_alliance, bg_system_horde,
-		monster_say, monster_party, monster_yell, monster_whisper, monster_emote,
-		raid_boss_whisper, raid_boss_emote,
-		skill, loot, money, opening, tradeskills, pet_info, combat_misc_info, combat_xp_gain, combat_honor_gain, combat_faction_change,
-]]
--- Configuration End
--- Do not change anything under this line except you know what you're doing (:
-
-
-
-local select = select
 local UP, DOWN = 1, -1
 
 local function getMinHeight(self)
 	local minHeight = 0
-	for i=1, minimizedLines do
+	for i=1, ns.cfg.minimizedLines do
 		minHeight = minHeight + math.floor(select(2, self:GetFont())+0.5) + (self:GetSpacing()*0.9)
 	end
 	return minHeight
 end
-		
---[[
-local function getMinHeight(self)
-	local minHeight = 0
-	for i=1, minimizedLines do
-		local line = select(9+i, self:GetRegions())
-		if(line) then
-			minHeight = minHeight + line:GetHeight() + 2.5
-		end
-	end
-	if(minHeight == 0) then
-		minHeight = select(2, self:GetFont()) + 2.5
-	end
-	return minHeight
-end
-]]
 
-local function Update(self, elapsed)
-	if(self.WaitTime) then
-		self.WaitTime = self.WaitTime - elapsed
-		if(self.WaitTime > 0) then return end
-		self.WaitTime = nil
-		if(self.Frozen) then return self:Hide() end
-	end
+local function Animate(chatframe, dir, waitTime, finishedFunc)
+	if chatframe.Frozen == true then return end
+	if chatframe.Animate == dir or chatframe.State == dir and not chatframe.Animate then return end
 
-	self.State = nil
-
-	self.TimeRunning = self.TimeRunning + elapsed
-	local animPercent = min(self.TimeRunning/animTime, 1)
-
-	local heightPercent = self.Animate == DOWN and 1-animPercent or animPercent
-
-	local minHeight = getMinHeight(ns.chicchai.Frame)
-	ns.chicchai.Frame:SetHeight(minHeight + (maxHeight-minHeight) * heightPercent)
-
-	if(animPercent >= 1) then
-		self.State = self.Animate
-		self.Animate = nil
-		self.TimeRunning = nil
-		self:Hide()
-		if(self.finishedFunc) then self:finishedFunc() end
-	end
-end
-
-local function getChicchai(self)
-	if(self:GetObjectType() == "Frame") then self = ns.chicchai.Frame  end
-	if(ns.chicchai.isDocked) then self = GENERAL_CHAT_DOCK.DOCKED_CHAT_FRAMES[1] end
-	return self.Chicchai
-end
-
-local function SetFrozen(self, isFrozen)
-	getChicchai(self).Frozen = isFrozen
-end
-
-local function Animate(self, dir, waitTime, finishedFunc)
-	local self = getChicchai(self)
-	if(self.Frozen) then return end
-	if(self.Animate == dir or self.State == dir and not self.Animate) then return end
-
-	if(self.Animate == -dir) then
-		self.TimeRunning = animTime - self.TimeRunning
+	if chatframe.Animate == -dir then
+		chatframe.TimeRunning = ns.cfg.animTime - chatframe.TimeRunning
 	else
-		self.TimeRunning = 0
+		chatframe.TimeRunning = 0
 	end
-	self.WaitTime = waitTime
-	self.Animate = dir
-	self.finishedFunc = finishedFunc
-	self:Show()
+	chatframe.WaitTime = waitTime
+	chatframe.Animate = dir
+	chatframe.finishedFunc = finishedFunc
 end
 
-local function Maximize(self) Animate(self, UP) end
-local function Minimize(self) Animate(self, DOWN) end
-
-local function MinimizeAfterWait(self)
-	Animate(self, DOWN, minimizeTime)
+local function MinimizeAfterWait(chatframe)
+	Animate(chatframe, DOWN, ns.cfg.minimizeTime)
 end
 
-local CheckEnterLeave
-if(MaximizeOnEnter) then
-	CheckEnterLeave = function(self)
-		self = getChicchai(self)
-		if(MouseIsOver(ns.chicchai.Frame) and not self.wasOver) then
-			self.wasOver = true
-			Animate(self, UP, WaitAfterEnter)
-		elseif(self.wasOver and not MouseIsOver(ns.chicchai.Frame)) then
-			self.wasOver = nil
-			Animate(self, DOWN, WaitAfterLeave)
+if ns.cfg.MaximizeCombatLog == true then
+	ns.cfg.ChatFrameConfig["ChatFrame2"] = true
+	hooksecurefunc("FCF_Tab_OnClick", function(tab)
+		local chatframe = _G["ChatFrame2"]
+		if chatframe.isDocked then
+			chatframe = GENERAL_CHAT_DOCK.primary
 		end
-	end
-end
+		if not chatframe then return end
 
-if(MaximizeCombatLog) then
-	hooksecurefunc("FCF_Tab_OnClick", function(self)
-		local frame = getChicchai(ChatFrame2)
-		if(not frame) then return end
-
-		if(self == ChatFrame2Tab) then
-			Animate(frame, UP)
-			SetFrozen(frame, true)
-		elseif(frame.Frozen) then
-			SetFrozen(frame, nil)
-			Animate(frame, DOWN)
+		if tab:GetName() == "ChatFrame2Tab"then
+			Animate(chatframe, UP)
+			chatframe.Frozen = true
+		elseif chatframe.Frozen then
+			chatframe.Frozen = false
+			Animate(chatframe, DOWN)
 		end
 	end)
 end
 
-local function updateHeight(self)
-	local self = getChicchai(self)
-	if(self.State ~= DOWN) then return end
-	ns.chicchai.Frame:ScrollToBottom()
-	ns.chicchai.Frame:SetHeight(getMinHeight(ns.chicchai.Frame))
+local function updateHeight(chatframe)
+	if chatframe.State ~= DOWN then return end
+	chatframe:ScrollToBottom()
+	chatframe:SetHeight(getMinHeight(chatframe))
 end
 
-local function chatEvent(self)
-	if(event == "CHAT_MSG_CHANNEL" and channelNumbers and not channelNumbers[arg8]) then return end
-
-	if(not LockInCombat or not UnitAffectingCombat("player")) then
-		Animate(self, UP, nil, MinimizeAfterWait)
+local function chatEvent(chatframe, ...)
+	local chatevent, message, _, _, _, _, _, number = ...
+	if event == "CHAT_MSG_CHANNEL" and ns.cfg.channelNumbers and not ns.cfg.channelNumbers[number] then return end
+	if chatframe.isDocked then
+		if type(ns.cfg.ChatFrameConfig[FCFDock_GetSelectedWindow(GENERAL_CHAT_DOCK):GetName()]) == "boolean" then return end
+		local checkevent = string.gsub(chatevent, "CHAT_MSG_", "")
+		for _, event in pairs(ns.cfg.ChatFrameConfig[FCFDock_GetSelectedWindow(GENERAL_CHAT_DOCK):GetName()]) do
+			if string.match(event, string.lower(checkevent)) then
+				if(ns.cfg.LockInCombat == false or not UnitAffectingCombat("player")) then
+					Animate(chatframe, UP, nil, MinimizeAfterWait)
+				end
+			end
+		end
+	else
+		if type(ns.cfg.ChatFrameConfig[chatframe:GetName()]) == "boolean" then return end
+		local checkevent = string.gsub(chatevent, "CHAT_MSG_", "")
+		for _, event in pairs(ns.cfg.ChatFrameConfig[chatframe:GetName()]) do
+			if string.match(event, string.lower(checkevent)) then
+				if(ns.cfg.LockInCombat == false or not UnitAffectingCombat("player")) then
+					Animate(chatframe, UP, nil, MinimizeAfterWait)
+				end
+			end
+		end
 	end
 end
 
-for chatname, options in pairs(ChatFrameConfig) do
+local function Update(chatframe, elapsed)
+	if chatframe.isDocked then
+		chatframe = GENERAL_CHAT_DOCK.primary
+	end
+	if ns.cfg.MaximizeOnEnter == true then
+		if MouseIsOver(chatframe) and not chatframe.wasOver then
+			chatframe.wasOver = true
+			Animate(chatframe, UP, ns.cfg.WaitAfterEnter)
+		elseif chatframe.wasOver and not MouseIsOver(chatframe) then
+			chatframe.wasOver = nil
+			Animate(chatframe, DOWN, ns.cfg.WaitAfterLeave)
+		end
+	end
+
+	if chatframe.WaitTime then
+		chatframe.WaitTime = chatframe.WaitTime - elapsed
+		if chatframe.WaitTime > 0 then return end
+		chatframe.WaitTime = nil
+	end
+
+	chatframe.State = nil
+
+	if chatframe.TimeRunning == nil then return end
+	chatframe.TimeRunning = chatframe.TimeRunning + elapsed
+	local animPercent = min(chatframe.TimeRunning/ns.cfg.animTime, 1)
+
+	local heightPercent = chatframe.Animate == DOWN and 1-animPercent or animPercent
+
+	local minHeight = getMinHeight(chatframe)
+	chatframe:SetHeight(minHeight + (ns.cfg.maxHeight-minHeight) * heightPercent)
+
+	if(animPercent >= 1) then
+		chatframe.State = chatframe.Animate
+			chatframe.Animate = nil
+			chatframe.TimeRunning = nil
+		if chatframe.finishedFunc then chatframe:finishedFunc() end
+	end
+end
+
+for chatname, options in pairs(ns.cfg.ChatFrameConfig) do
 	local chatframe = _G[chatname]
-	ns.chicchai = CreateFrame("Frame")
-	ns.chicchai:RegisterEvent("ADDON_LOADED")
-	if(MaximizeOnEnter) then
-		local updater = CreateFrame("Frame", nil, chatframe)
-		updater:SetScript("OnUpdate", CheckEnterLeave)
-		updater.Frame = chatframe
-	end
-	ns.chicchai.Frame = chatframe
-	chatframe.Chicchai = ns.chicchai
-	if(type(options) == "table") then
+
+	if type(options) == "table" then
 		for _, event in pairs(options) do
 			if(not event:match("[A-Z]")) then
 				event = "CHAT_MSG_"..event:upper()
 			end
-			ns.chicchai:RegisterEvent(event)
+			chatframe:RegisterEvent(event)
 		end
 	end
-	ChatFrameConfig[chatname] = ns.chicchai
-	
-	chatframe.Maximize = Maximize
-	chatframe.Minimize = Minimize
-	chatframe.UpdateHeight = updateHeight
-	chatframe.SetFrozen = SetFrozen
 
-	ns.chicchai:SetScript("OnUpdate", Update)
-	ns.chicchai:SetScript("OnEvent", chatEvent)
-	ns.chicchai:Hide()
+	chatframe:SetScript("OnUpdate", Update)
+	chatframe:HookScript("OnEvent", chatEvent)
 
 	updateHeight(chatframe)
-
-	hooksecurefunc(chatframe, "AddMessage", updateHeight)
-end
-
-_G.Chicchai = ChatFrameConfig
-
-function ns.chicchai:ADDON_LOADED(event, addon)
-	if addon == "Chicchai" then
-		Minimize()
-	end
 end
